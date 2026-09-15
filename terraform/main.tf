51d71f95d3f64037cd002cfe680f2d51226bb137
Kash571-PCBPO <kashmithamadushan571@gmail.com>
2026-09-15 10:23:00 +0530

    chore: add comment to clarify block_public_acls setting in S3 bucket configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ resource "aws_s3_bucket" "website" {
 resource "aws_s3_bucket_public_access_block" "website" {
     bucket = aws_s3_bucket.website.id
 
-    block_public_acls = true
+    block_public_acls = true # ACL - Access Control List
     block_public_policy = true
     ignore_public_acls = true
     restrict_public_buckets = true
